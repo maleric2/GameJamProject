be409e8a53cf058a516382da0c7a62ec740252d6
Toni Steyskal <toni.steyskal@gmail.com>
2015-11-15 00:53:02 +0100

    MainMenu Update

---
+++ GameJamProject/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void NewGame()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void Credits()
+    {
+        //TO DO
+        // implement Credits canvas and switch controller
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ GameJamProject/Assets/Scripts/SliderOnMouseOverFill.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SliderOnMouseOverFill : MonoBehaviour {
+
+    private Slider buttonSlider;
+
+	// Use this for initialization
+	void Start () {
+        buttonSlider = gameObject.GetComponent<Slider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnMouseOver()
+    {
+        Debug.Log("TWST");
+        
+    }
+}
