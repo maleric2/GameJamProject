b8ed956d6cc3ea2fcf250bc6bc576e476e2112d7
Toni Steyskal <toni.steyskal@gmail.com>
2015-11-14 15:44:18 +0100

    Player Prefab - Test Scene

---
+++ GameJamProject/Assets/Scripts/BulletExplosion.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletExplosion : MonoBehaviour {
+
+    public LayerMask enemyMask;                        // Enemies layer
+    //public ParticleSystem m_ExplosionParticles;         
+    //public AudioSource m_ExplosionAudio;              
+    public float maxDamage = 100.0f;                    
+    public float explosionForce = 1000.0f;              
+    public float maxLifeTime = 2.0f;                    
+    public float explosionRadius = 5.0f;               
+
+    private void Start()
+    {
+        Destroy(gameObject, maxLifeTime);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Collect all the colliders in a sphere from the shell's current position to a radius of the explosion radius.
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius, enemyMask);
+
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
+
+            if (!targetRigidbody)
+                continue;
+
+            targetRigidbody.AddExplosionForce(explosionForce, transform.position, explosionRadius);
+            HealthController targetHealth = targetRigidbody.GetComponent<HealthController>();
+
+            if (!targetHealth)
+                continue;
+
+            float damageAmount = CalculateDamage(targetRigidbody.position);
+            targetHealth.TakeDamage(damageAmount);
+        }
+
+        // Unparent the particles from the shell.
+        //m_ExplosionParticles.transform.parent = null;
+
+        // Play the particle system.
+        //m_ExplosionParticles.Play();
+
+        // Play the explosion sound effect.
+        //m_ExplosionAudio.Play();
+
+        // Once the particles have finished, destroy the gameobject they are on.
+        //Destroy(m_ExplosionParticles.gameObject, m_ExplosionParticles.duration);
+
+        Destroy(gameObject);
+    }
+
+
+    private float CalculateDamage(Vector3 targetPosition)
+    {
+        Vector3 explosionToTarget = targetPosition - transform.position;
+        float explosionDistance = explosionToTarget.magnitude;
+        float relativeDistance = (explosionRadius - explosionDistance) / explosionRadius;
+        float damage = relativeDistance * maxDamage;
+        damage = Mathf.Max(0f, damage);
+
+        return damage;
+    }
+}
---
+++ GameJamProject/Assets/Scripts/HealthController.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthController : MonoBehaviour {
+
+    public float startingHealth = 100.0f;               
+    public Slider healthSlider;                             
+    public Image healthImage;                           
+    public Color fullHealthColor = Color.green;       
+    public Color zeroHealthColor = Color.red;         
+    //public GameObject explosionPrefab;                
+
+    //private AudioSource m_ExplosionAudio;               // The audio source to play when the tank explodes.
+    //private ParticleSystem m_ExplosionParticles;        // The particle system the will play when the tank is destroyed.
+    private float currentHealth;                      
+    private bool isDead;                                
+
+
+    private void Awake()
+    {
+        // Instantiate the explosion prefab and get a reference to the particle system on it.
+        //m_ExplosionParticles = Instantiate(explosionPrefab).GetComponent<ParticleSystem>();
+
+        // Get a reference to the audio source on the instantiated prefab.
+        //m_ExplosionAudio = m_ExplosionParticles.GetComponent<AudioSource>();
+
+        // Disable the prefab so it can be activated when it's required.
+        //m_ExplosionParticles.gameObject.SetActive(false);
+
+        currentHealth = startingHealth;
+        isDead = false;
+
+        SetHealthUI();
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+        currentHealth -= damageAmount;
+        
+        SetHealthUI();
+
+        if (currentHealth <= 0.0f && !isDead)
+        {
+            OnDeath();
+        }
+    }
+
+
+    private void SetHealthUI()
+    {
+        healthSlider.value = currentHealth;
+
+        // Interpolate the color of the bar between the choosen colours based on the current percentage of the starting health.
+        healthImage.color = Color.Lerp(zeroHealthColor, fullHealthColor, currentHealth / startingHealth);
+    }
+
+
+    private void OnDeath()
+    {
+        isDead = true;
+
+        // Move the instantiated explosion prefab to the tank's position and turn it on.
+        //m_ExplosionParticles.transform.position = transform.position;
+        //m_ExplosionParticles.gameObject.SetActive(true);
+
+        // Play the particle system of the tank exploding.
+        //m_ExplosionParticles.Play();
+
+        // Play the tank explosion sound effect.
+        //m_ExplosionAudio.Play();
+
+        // Turn the tank off.
+        gameObject.SetActive(false);
+    }
+}
