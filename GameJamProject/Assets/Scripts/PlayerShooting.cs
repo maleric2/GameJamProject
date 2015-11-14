653cc48bd9bc263db5466f6342a7c0d08ced114a
Toni Steyskal <toni.steyskal@gmail.com>
2015-11-14 16:38:34 +0100

    Player - Audio

--- GameJamProject/Assets/Scripts/PlayerShooting.cs
+++ GameJamProject/Assets/Scripts/PlayerShooting.cs
@@ -7,9 +7,9 @@ public class PlayerShooting : MonoBehaviour {
     public Rigidbody bulletPrefab;                   
     public Transform fireTransformPoint;          
     public Slider aimSlider;                
-    //public AudioSource m_ShootingAudio;
-    //public AudioClip m_ChargingClip;            
-    //public AudioClip m_FireClip;                
+    public AudioSource shootingAudioSource;
+    public AudioClip chargingClip;            
+    public AudioClip fireClip;                
     public float minLaunchForce = 15.0f;        
     public float maxLaunchForce = 30.0f;        
     public float maxChargeTime = 0.75f;       
@@ -38,6 +38,11 @@ public class PlayerShooting : MonoBehaviour {
             // ... use the max force and launch the shell.
             currentLaunchForce = maxLaunchForce;
             Fire();
+
+            // If it plays charge afterwards, it never plays the Fire clip!
+            //shootingAudioSource.clip = chargingClip;
+            //shootingAudioSource.Play();
+
             isFired = false;
         }
         // Otherwise, if the fire button has just started being pressed...
@@ -48,8 +53,8 @@ public class PlayerShooting : MonoBehaviour {
             currentLaunchForce = minLaunchForce;
 
             // Change the clip to the charging clip and start it playing.
-            //m_ShootingAudio.clip = m_ChargingClip;
-            //m_ShootingAudio.Play();
+            shootingAudioSource.clip = chargingClip;
+            shootingAudioSource.Play();
         }
         // Otherwise, if the fire button is being held and the shell hasn't been launched yet...
         else if (Input.GetMouseButton(0) && !isFired)
@@ -76,8 +81,8 @@ public class PlayerShooting : MonoBehaviour {
         shellInstance.velocity = currentLaunchForce * fireTransformPoint.forward; ;
 
         // Change the clip to the firing clip and play it.
-        //m_ShootingAudio.clip = m_FireClip;
-        //m_ShootingAudio.Play();
+        shootingAudioSource.clip = fireClip;
+        shootingAudioSource.Play();
 
         // Reset the launch force.  This is a precaution in case of missing button events.
         currentLaunchForce = minLaunchForce;
