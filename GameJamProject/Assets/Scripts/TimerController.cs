846db8048c87cfc8d4009c16923b810ccd257ea6
Toni Steyskal <toni.steyskal@gmail.com>
2015-11-15 01:12:35 +0100

    GameplayCanvas, TimeController

---
+++ GameJamProject/Assets/Scripts/TimerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour {
+
+    public Text timerTexT;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.timeSinceLevelLoad >= 5.0f)
+            timerTexT.text = "Time since last Reboot: " + Mathf.Round(Time.timeSinceLevelLoad);
+	}
+}
