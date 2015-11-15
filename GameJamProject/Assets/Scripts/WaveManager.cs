405be990cfe9a177fe6404991decfe2ee9eaf8b2
Marko Alerić <maleric@foi.hr>
2015-11-15 09:02:00 +0100

    Mozda fixano targets, malo cudna kretanja healer botova

--- GameJamProject/Assets/Scripts/WaveManager.cs
+++ GameJamProject/Assets/Scripts/WaveManager.cs
@@ -5,11 +5,14 @@ using System.Collections.Generic;
 public class WaveManager : MonoBehaviour
 {
     public static WaveManager wm;
+    public int registeredSpawners = 0;
+    public int spawnersCompletedWave = 0;
+
     public float secondsBetweenWaves = 20.0f;
     public float secondsBetweenSpawning = 3f;
     public int maxNumberOfWaves;
     //public List<int> waveTargetNumber;
-    public int currentTarget;
+    public int currentTarget = -1;
 
     public int currentWave = 1;
 
@@ -18,14 +21,14 @@ public class WaveManager : MonoBehaviour
 
     public int currentWaveSpawner = 0;
     public GameManager gameManager;
+    public bool newWaveGenerated = false;
     // Use this for initialization
     void Start()
     {
-        if(wm == null)
+        if (wm == null)
             wm = gameObject.GetComponent<WaveManager>();
 
         gameManager = GameObject.FindObjectOfType<GameManager>();
-        //waveTargetNumber = new List<int>();
     }
 
     // Update is called once per frame
@@ -35,28 +38,35 @@ public class WaveManager : MonoBehaviour
     }
     public Transform GetWaveTarget()
     {
-        if (currentWave == 0) currentWave = 1;
-        /*if (waveTargetNumber.Count < (currentWave)) //if there is new wave
-        {
+        /*Debug.Log("GetWaveTarget");
+
+        if (currentTarget < 0)
             GetRandomTarget();
-            currentWave = waveTargetNumber.Count;
-        }*/
-        if (gameManager.targets.Count> currentTarget && gameManager.targets[currentTarget] != null)
+        if (currentWave == 0) currentWave = 1;
+
+        if (gameManager.targets.Count > currentTarget && gameManager.targets[currentTarget] != null) { 
+            
+            if(gameManager.targets[currentTarget].GetComponent<HealthController>().isFullHealth())
+                GetRandomTarget();
             return gameManager.targets[currentTarget];
+        }
         else
         {
             GetRandomTarget();
             return gameManager.targets[currentTarget];
-        }
+        }*/
+        return gameManager.targets[currentTarget];
     }
 
-   /* private void GetRandomTarget(int index)
+    /* private void GetRandomTarget(int index)
+     {
+         waveTargetNumber.Insert(index, SetRandomTarget());
+     }*/
+    public void GetRandomTarget()
     {
-        waveTargetNumber.Insert(index, SetRandomTarget());
-    }*/
-    private void GetRandomTarget()
-    {
-        currentTarget = SetRandomTarget();
+        if(!newWaveGenerated)
+            currentTarget = SetRandomTarget();
+
     }
     private int SetRandomTarget()
     {
@@ -64,29 +74,50 @@ public class WaveManager : MonoBehaviour
         int targetNumber;
         do
         {
-            defined = true;
+            defined = false;
             targetNumber = Random.Range(0, gameManager.targets.Count);
+            Debug.Log("Random: " + targetNumber);
 
             if (GameManager.gm.targets.Count > 0)
             {
-                if (GameManager.gm.targets[targetNumber] == null) defined = false;
+                if (GameManager.gm.targets.Count > targetNumber && GameManager.gm.targets[targetNumber] != null)
+                {
+                    defined = true;
+                    Debug.Log("Target Good");
+                }
             }
-            else Debug.Log("No targets");
+            else Debug.Log("No targets (ERROR)");
 
         } while (!defined);
-
+        newWaveGenerated = true;
         Debug.Log("New Wave Target defined");
         return targetNumber;
     }
     public void makeWave(int wave)
     {
+
         if (wave > currentWave)
         {
+            newWaveGenerated = false;
+            GetRandomTarget();
             startWaveNumber += waveIncrement;
+            spawnersCompletedWave = 0;
             currentWave = wave;
             Debug.Log("Get ready for wave " + currentWave);
-            GetRandomTarget();
         }
     }
 
+    public void RegisterSpawner()
+    {
+        registeredSpawners++;
+    }
+    public void ICompletedWave()
+    {
+        spawnersCompletedWave++;
+    }
+    public bool SpawnersCompletedWave()
+    {
+        if (registeredSpawners <= spawnersCompletedWave) return true;
+        else return false;
+    }
 }
